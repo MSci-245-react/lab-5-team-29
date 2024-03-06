e581e6e14b94357e46ba724575285ca8a5039c2f
chris0517 <chris92563@outlook.com>
2024-03-06 11:32:07 -0500

    complete lab 8

--- src/components/RestaurantScreen.js
+++ src/components/RestaurantScreen.js
@@ -1,11 +1,19 @@
 import RestaurantList from './RestaurantList';
 
 export default function RestaurantScreen() {
+  const restaurants = [
+    { id: 1, name: 'Sushi Place' },
+    { id: 2, name: 'Pizza Place' },
+  ];
+
+  const loadRestaurants = () => {
+    // Implement logic to load restaurants
+  };
+
   return (
     <div>
-      {' '}
       <h1>Restaurants</h1>
-      <RestaurantList />
+      <RestaurantList loadRestaurants={loadRestaurants} restaurants={restaurants} />
     </div>
   );
 }
