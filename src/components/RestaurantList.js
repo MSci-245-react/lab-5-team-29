047fa5f3d0450b4d208a83a39be10bc808f33336
Ayush Bhargava <62566974+ayushbh2020@users.noreply.github.com>
2024-03-13 22:13:09 -0400

    lab 1- stuf

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,6 +1,7 @@
 import {useEffect} from 'react';
-
-export default function RestaurantList({loadRestaurants, restaurants}) {
+import {connect} from 'react-redux';
+import {loadRestaurants} from '../store/restaurants/actions';
+export function RestaurantList({loadRestaurants, restaurants}) {
   useEffect(() => {
     loadRestaurants();
   }, [loadRestaurants]);
@@ -13,3 +14,11 @@ export default function RestaurantList({loadRestaurants, restaurants}) {
     </ul>
   );
 }
+
+const mapStateToProps = state => ({
+  restaurants: state.restaurants.records,
+});
+
+const mapDispatchToProps = {loadRestaurants};
+
+export default connect(mapStateToProps, mapDispatchToProps)(RestaurantList);
